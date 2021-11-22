633827487b1b5ec7d7a0711e2dfbee45e017ce16
Zachary Brown <studio13productions@outlook.com>
2021-11-21 19:24:49 -0500

    updated packe.json for nodemon, added guild based slash commands

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
+
 //New Instance of the Discord Bot Client
 const client = new Client ({ intents : [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
@@ -12,8 +13,41 @@ client.once('ready', ()=>{
 
 client.on("messageCreate", message=>{
     if (message.author.bot) return false;
-
     console.log(`Message from ${message.author.username}: ${message.content}`);
+    
+    const guildId = '810590335557566504'
+    const guild = client.guilds.cache.get(guildId)
+    
+    let commands
+    if (guild) {
+        commands = guild.commands
+    }
+    else {
+        commands = client.application?.commands    
+    }
+     commands?.create({
+        name: 'gping',
+        description: 'replies with ping',
+
+    })
+})
+client.on('interactionCreate', async (interaction)=>{
+    if (!interaction.isCommand()){
+        return
+    }
+    const {commandName, options} = interaction
+
+    if (commandName=== 'gping'){
+        interaction.reply({
+            content:'gpong',
+            ephemeral:true,
+
+            
+        })
+
+        }
+    
+
 })
     
 
