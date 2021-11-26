36a0c5463715f71b7b8fc732c37280e21767879b
Zachary Brown <studio13productions@outlook.com>
2021-11-25 19:13:16 -0500

    Added Command Handler and /Commands Directory

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const {Client, Intents } = require('discord.js');
+const {Client, Collection, Intents} = require('discord.js');
 const { token } = require('./config.json');
 const mongoose = require('mongoose');
-const testSchema = require('./test-schema')
+const testSchema = require('./test-schema');
+const fs = require('fs');
+
+
 
 
 //New Instance of the Discord Bot Client
 const client = new Client ({ intents : [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
 
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file=>file.endsWith('.js'));
+
+for (const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.data.name, command);
+}
 //When the client is ready, run this code once
 client.once('ready', ()=>{
     console.log('Ready!');
-})
+});
 
 client.on('ready', async ()=>{
     await mongoose.connect(
@@ -24,7 +35,7 @@ setTimeout(async ()=>{
     await new testSchema({
         message:'Goodbye World',
     }).save()
-}, 1000)  
+}, 1000)
 
 
 client.on("messageCreate", async message=>{
@@ -33,69 +44,37 @@ client.on("messageCreate", async message=>{
     const userInput = message.content
 
     console.log(`Message from ${message.author.username}: ${userInput}`);
+})
+
+    client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } 
+        
+        catch(error)
+    {
+        console.error(error);
+        return interaction.reply ({content:'Command Execution Error!', ephemeral:true});
+    }
+})
     
     //You can use this function to write to your database anywhere
     //Commented out because it writes everytime you type something
    /* await new testSchema({
         message:`${userInput}`,
     }).save()*/
+  
 
 
     
-    const guildId = '810590335557566504'
-    const guild = client.guilds.cache.get(guildId)
+
     
-    let commands
-    if (guild) {
-        commands = guild.commands
-    }
-    else {
-        commands = client.application?.commands    
-    }
-     commands?.create({
-        name: 'gping',
-        description: 'replies with ping',
-        name: 'roll',
-        description: 'dice roller for world director',
-
-    })
-})
-client.on('interactionCreate', async (interaction)=>{
-    if (!interaction.isCommand()){
-        return
-    }
-    const {commandName, options} = interaction
-
-    if (commandName=== 'gping'){
-        interaction.reply({
-            content:'gpong',
-            ephemeral:true,
-
-            
-        })}
-    else if (commandName === 'roll'){
-        var dice = {
-            sides: 6,
-            roll: function (){
-                var randomNumber = Math.floor(Math.random() * this.sides) +1;
-                return randomNumber;
-            }
-            
-        }
-        rollResult = dice.roll();
-        interaction.reply({
-            content:`You rolled a ${rollResult}!`,
-            ephemeral:false,
-        })
-
-    }
-    
-
-        
-      
-    
-
-})
     
 
 //login to discord using the token referenced in config
